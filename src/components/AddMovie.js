074936c4d89d852a8915059b3baa6ea9121a670c
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-12 19:15:22 +0200

    Final example

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -34,36 +34,16 @@ function AddMovie(props) {
       <h4>Add a Movie</h4>
       <form onSubmit={handleSubmit}>
         <label>Title:</label>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={handleTitleInput}
-        />
+        <input type="text" name="title" value={title} onChange={handleTitleInput} />
 
         <label>Director:</label>
-        <input
-          type="text"
-          name="director"
-          value={director}
-          onChange={handleDirectorInput}
-        />
+        <input type="text" name="director" value={director} onChange={handleDirectorInput} />
 
         <label>IMDB Rating:</label>
-        <input
-          type="number"
-          name="IMDBRating"
-          value={IMDBRating}
-          onChange={handleRatingInput}
-        />
+        <input type="number" name="IMDBRating" value={IMDBRating} onChange={handleRatingInput} />
 
         <label>Won Oscars:</label>
-        <input
-          type="checkbox"
-          name="hasOscars"
-          checked={hasOscars}
-          onChange={handleOscarsInput}
-        />
+        <input type="checkbox" name="hasOscars" checked={hasOscars} onChange={handleOscarsInput} />
         <button type="submit">Add a Movie</button>
       </form>
     </div>
